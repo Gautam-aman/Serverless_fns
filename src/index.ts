7c35f84397eb42cbc995fa55ac66c1e0018df1f9
Aman Gautam <amangautam2128@gmail.com>
2024-07-16 23:09:32 +0530

    deployed

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,34 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.toml`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+export interface Env{
+
+}
+
+/*
+export default {
+	async fetch(request:Request, env : Env , ctx:ExecutionContext):Promise<Response>{
+		console.log (request.body)
+		console.log(request.headers)
+		console.log(request.method)
+
+		if (request.method == "GET"){
+			return Response.json({
+				message : "Response Sended Successfully"
+			})
+		}
+		else{
+			return Response.json({
+				message : "Response not sended successfully"
+			})
+		}
+	}
+};
+
+*/
 
 export default {
-	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
-	},
-} satisfies ExportedHandler<Env>;
+	async fetch(request:Request, env : Env , ctx:ExecutionContext):Promise<Response>{
+			return Response.json({
+				message : "Aman Gautam is Great"
+			})
+		
+	}
+};
